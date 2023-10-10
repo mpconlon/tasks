3b1a5918e9c47da9bc40031d57f67560c636f3ca
Matthew Conlon <mpconlon@udel.edu>
2023-10-09 21:11:59 -0400

    Added hello world

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,9 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript - Matthew Conlon
+                UD CISC275 App - Matthew Conlon
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <p>Hello World!</p>
         </div>
     );
 }
