ae7a1ef4f012103501a7db0698d8f78d43546d9f
Matthew Conlon <mpconlon@udel.edu>
2023-10-26 18:18:12 -0400

    build error

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,19 @@ function App(): JSX.Element {
                 UD CISC275 App - Matthew Conlon
             </header>
             <hr></hr>
+            <CheckAnswer expectedAnswer="42"></CheckAnswer>
+            <hr></hr>
+            <GiveAttempts></GiveAttempts>
+            <hr></hr>
+            <EditMode></EditMode>
+            <hr></hr>
+            <ChangeColor></ChangeColor>
+            <hr></hr>
+            <MultipleChoiceQuestion
+                options={["a", "b", "c"]}
+                expectedAnswer="b"
+            ></MultipleChoiceQuestion>
+            <hr></hr>
             {/* <DoubleHalf></DoubleHalf> */}
             <hr></hr>
             <ChooseTeam></ChooseTeam>
