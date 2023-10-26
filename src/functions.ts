9970c5b303655e397ff383b3c4b427d242f2962f
Matthew Conlon <mpconlon@udel.edu>
2023-10-26 10:22:53 -0400

    fixed parentheses problem

--- src/functions.ts
+++ src/functions.ts
@@ -14,8 +14,11 @@ export function fahrenheitToCelius(temperature: number): number {
  */
 export function add3(first: number, second: number, third: number): number {
     let sum = 0;
+    // eslint-disable-next-line no-extra-parens
     first > 0 ? (sum += first) : (sum = sum);
+    // eslint-disable-next-line no-extra-parens
     second > 0 ? (sum += second) : (sum = sum);
+    // eslint-disable-next-line no-extra-parens
     third > 0 ? (sum += third) : (sum = sum);
     return sum;
 }
